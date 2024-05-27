e94052efcbbb7af0d1460a7caceed3ba2548bda1
Hallison Batista <hallisonbatista@gmail.com>
2024-05-27 16:16:26 -0400

    Classe para proponentes

---
+++ desconto_inss/db/migrate/20240527135856_create_proponents.rb
@@ -0,0 +1,20 @@
+class CreateProponents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :proponents do |t|
+      t.string :name
+      t.string :document_br_cpf, limit: 11
+      t.date :birth_date
+      t.string :address_public_place
+      t.string :address_number
+      t.string :address_district
+      t.string :address_city
+      t.string :address_state, limit: 2
+      t.string :address_postalcode, limit: 8
+      t.string :phone_contact, limit: 16
+      t.string :phone_reference, limit: 16
+      t.integer :salary
+
+      t.timestamps
+    end
+  end
+end
