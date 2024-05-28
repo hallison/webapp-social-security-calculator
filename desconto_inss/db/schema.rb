2caa9dbb96f032654b538f9c710faa6dd5709a5b
Hallison Batista <hallisonbatista@gmail.com>
2024-05-28 12:04:59 -0400

    Atualização do esquema para tabela de proponentes

---
+++ desconto_inss/db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_135856) do
+  create_table "proponents", force: :cascade do |t|
+    t.string "name"
+    t.string "document_br_cpf", limit: 11
+    t.date "birth_date"
+    t.string "address_public_place"
+    t.string "address_number"
+    t.string "address_district"
+    t.string "address_city"
+    t.string "address_state", limit: 2
+    t.string "address_postalcode", limit: 8
+    t.string "phone_contact", limit: 16
+    t.string "phone_reference", limit: 16
+    t.integer "salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
