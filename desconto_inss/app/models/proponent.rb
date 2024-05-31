355deb8dcc8fb03e082d67f7b32ecb550b1126e1
Hallison Batista <hallisonbatista@gmail.com>
2024-05-31 14:21:46 -0400

    Cálculo da contribuição social

--- desconto_inss/app/models/proponent.rb
+++ desconto_inss/app/models/proponent.rb
@@ -7,4 +7,23 @@ class Proponent < ApplicationRecord
   validates :document_br_cpf, presence: true, length: { is: 11 }
   validates :birth_date, presence: true
   validates :salary_gross, presence: true
+
+  def social_contribution
+    total_contribution salary
+  end
+
+  def update_salary_net!
+    update_salary_net.save!
+  end
+
+  def update_salary_net
+    self[:salary_net] = (salary - social_contribution) * 100 # Money
+    self
+  end
+
+  private
+
+  def salary
+    salary_gross / 100.0 # Money
+  end
 end
