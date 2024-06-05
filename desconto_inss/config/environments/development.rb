bf345720a4614abcd9a315847b2ad5b0579d3414
Hallison Batista <hallisonbatista@gmail.com>
2024-06-05 15:33:20 -0400

    Remoção do Pry para execução do Sidekiq com Docker

--- desconto_inss/config/environments/development.rb
+++ desconto_inss/config/environments/development.rb
@@ -1,6 +1,4 @@
 require "active_support/core_ext/integer/time"
-require "pry"
-require "byebug"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
