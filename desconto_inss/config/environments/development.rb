417bbacf1d2ca5497e90123104b4b1af589f0b2d
Hallison Batista <hallisonbatista@gmail.com>
2024-06-05 10:24:47 -0400

    Byebug

--- desconto_inss/config/environments/development.rb
+++ desconto_inss/config/environments/development.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+require "pry"
+require "byebug"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
